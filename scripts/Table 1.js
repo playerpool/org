7f6ca11cb3af3d4f96214d7a8795aafb48002d0d
playerpool <50332306+playerpool@users.noreply.github.com>
2019-06-01 15:01:36 -0400

    Add files via upload

---
+++ scripts/Table 1.js
@@ -0,0 +1,260 @@
+var dataset;
+var headers;
+var row;
+var clicks = {year: 0, person: 0, player: 0, school: 0, seed: 0, totalpoints: 0, gamesplayed:0, pick:0};
+var tabulate = function (data,columns) {
+    var table = d3.select('.chart').append('table')
+    var thead = table.append('thead')
+    var tbody = table.append('tbody')
+    var column_names = ["Year","Person","Player","School","Seed","Points","Games","Pick"];
+
+    var header = thead.append('tr')
+      .selectAll('th')
+        .data(column_names)
+        .enter()
+      .append('th')
+        .text(function (d) { return d; })
+
+    headers=header;
+
+    var rows = tbody.selectAll('tr')
+        .data(data)
+        .enter()
+      .append('tr')
+
+    row=rows;
+
+    var cells = rows.selectAll('td')
+        .data(function(row) {
+            return columns.map(function (column) {
+                return { column: column, value: row[column] };
+          });
+      })
+      .enter()
+    .append('td')
+      .text(function (d) { return d.value })
+
+  return table;
+};
+
+var data = d3.csv("../data/pp.csv", function(d) {
+    return {
+        year: +d.year,
+        person: d.person,
+        totalpoints: +d.totalpoints,
+        player: d.player,
+        school: d.school,
+        seed: +d.seed,
+        gamesplayed: +d.gamesplayed,
+        pick: +d.overallpick
+    };
+}).then(function (data) {
+    dataset = data
+    console.log(dataset)
+    var columns = ["year","person","player","school","seed","totalpoints","gamesplayed","pick"]
+    tabulate(data,columns)
+
+     headers
+    .on("click", function(d) {
+        if (d == "Person") {
+            clicks.person++;
+            // even number of clicks
+            if (clicks.person % 2 == 0) {
+              // sort ascending: alphabetically
+              row.sort(function(a,b) { 
+                if (a.person.toUpperCase() < b.person.toUpperCase()) { 
+                  return -1; 
+                } else if (a.person.toUpperCase() > b.person.toUpperCase()) { 
+                  return 1; 
+                } else {
+                  return 0;
+                }
+              });
+            // odd number of clicks  
+            } else if (clicks.person % 2 != 0) { 
+              // sort descending: alphabetically
+              row.sort(function(a,b) { 
+                if (a.person.toUpperCase() < b.person.toUpperCase()) { 
+                  return 1; 
+                } else if (a.person.toUpperCase() > b.person.toUpperCase()) { 
+                  return -1; 
+                } else {
+                  return 0;
+                }
+              });
+            }
+        }
+        if (d == "Player") {
+            clicks.player++;
+            if (clicks.player % 2 == 0) {
+              row.sort(function(a,b) { 
+                if (a.player.toUpperCase() < b.player.toUpperCase()) { 
+                  return -1; 
+                } else if (a.player.toUpperCase() > b.player.toUpperCase()) { 
+                  return 1; 
+                } else {
+                  return 0;
+                }
+              });
+            } else if (clicks.player % 2 != 0) { 
+              row.sort(function(a,b) { 
+                if (a.player.toUpperCase() < b.player.toUpperCase()) {  
+                  return 1; 
+                } else if (a.player.toUpperCase() > b.player.toUpperCase()) {  
+                  return -1; 
+                } else {
+                  return 0;
+                }
+              });
+            }
+        }
+        if (d == "School") {
+            clicks.school++;
+            if (clicks.school % 2 == 0) {
+              row.sort(function(a,b) { 
+                if (a.school.toUpperCase() < b.school.toUpperCase()) { 
+                  return -1; 
+                } else if (a.school.toUpperCase() > b.school.toUpperCase()) { 
+                  return 1; 
+                } else {
+                  return 0;
+                }
+              });
+            } else if (clicks.school % 2 != 0) { 
+              row.sort(function(a,b) { 
+                if (a.school.toUpperCase() < b.school.toUpperCase()) {  
+                  return 1; 
+                } else if (a.school.toUpperCase() > b.school.toUpperCase()) {  
+                  return -1; 
+                } else {
+                  return 0;
+                }
+              });
+            }
+        }
+        if (d == "Year") {
+            clicks.year++;
+            // even number of clicks
+            if (clicks.year % 2 == 0) {
+              // sort ascending: numerically
+              row.sort(function(a,b) { 
+                if (+a.year < +b.year) { 
+                  return -1; 
+                } else if (+a.year > +b.year) { 
+                  return 1; 
+                } else {
+                  return 0;
+                }
+              });
+            // odd number of clicks  
+            } else if (clicks.year % 2 != 0) { 
+              // sort descending: numerically
+              row.sort(function(a,b) { 
+                if (+a.year < +b.year) { 
+                  return 1; 
+                } else if (+a.year > +b.year) { 
+                  return -1; 
+                } else {
+                  return 0;
+                }
+              });
+            }
+          }
+        if (d == "Seed") {
+            clicks.seed++;
+            if (clicks.seed % 2 == 0) {
+              row.sort(function(a,b) { 
+                if (+a.seed < +b.seed) { 
+                  return -1; 
+                } else if (+a.seed > +b.seed) { 
+                  return 1; 
+                } else {
+                  return 0;
+                }
+              }); 
+            } else if (clicks.seed % 2 != 0) { 
+              row.sort(function(a,b) { 
+                if (+a.seed < +b.seed) { 
+                  return 1; 
+                } else if (+a.seed > +b.seed) { 
+                  return -1; 
+                } else {
+                  return 0;
+                }
+              });
+            }
+          }
+        if (d == "Points") {
+            clicks.totalpoints++;
+            if (clicks.totalpoints % 2 == 0) {
+              row.sort(function(a,b) { 
+                if (+a.totalpoints < +b.totalpoints) { 
+                  return -1; 
+                } else if (+a.totalpoints > +b.totalpoints) { 
+                  return 1; 
+                } else {
+                  return 0;
+                }
+              }); 
+            } else if (clicks.totalpoints % 2 != 0) { 
+              row.sort(function(a,b) { 
+                if (+a.totalpoints < +b.totalpoints) { 
+                  return 1; 
+                } else if (+a.totalpoints > +b.totalpoints) { 
+                  return -1; 
+                } else {
+                  return 0;
+                }
+              });
+            }
+          }
+        if (d == "Games") {
+            clicks.gamesplayed++;
+            if (clicks.gamesplayed % 2 == 0) {
+              row.sort(function(a,b) { 
+                if (+a.gamesplayed < +b.gamesplayed) { 
+                  return -1; 
+                } else if (+a.gamesplayed > +b.gamesplayed) { 
+                  return 1; 
+                } else {
+                  return 0;
+                }
+              }); 
+            } else if (clicks.gamesplayed % 2 != 0) { 
+              row.sort(function(a,b) { 
+                if (+a.gamesplayed < +b.gamesplayed) { 
+                  return 1; 
+                } else if (+a.gamesplayed > +b.gamesplayed) { 
+                  return -1; 
+                } else {
+                  return 0;
+                }
+              });
+            }
+          }
+        if (d == "Pick") {
+            clicks.pick++;
+            if (clicks.pick % 2 == 0) {
+              row.sort(function(a,b) { 
+                if (+a.pick < +b.pick) { 
+                  return -1; 
+                } else if (+a.pick > +b.pick) { 
+                  return 1; 
+                } else {
+                  return 0;
+                }
+              }); 
+            } else if (clicks.pick % 2 != 0) { 
+              row.sort(function(a,b) { 
+                if (+a.pick < +b.pick) { 
+                  return 1; 
+                } else if (+a.pick > +b.pick) { 
+                  return -1; 
+                } else {
+                  return 0;
+                }
+              });
+            }
+          }
+     });
+});
---
+++ scripts/step chart.js
@@ -0,0 +1,141 @@
+var dataset,
+    margin = {top: 30, right: 30, bottom: 30, left: 50},
+    width = 860 - margin.left - margin.right,
+    height = 400 - margin.top - margin.bottom;
+
+var svg = d3.select(".chart")
+  .append("svg")
+    .attr("width", width + margin.left + margin.right)
+    .attr("height", height + margin.top + margin.bottom)
+  .append("g")
+    .attr("transform",
+          "translate(" + margin.left + "," + margin.top + ")");
+
+var data = d3.csv("../data/cumrank.csv", function(d) {
+    return {
+            year: +d.year,
+            person: d.person,
+            cumrank: +d.cumrank,
+        };
+    }).then(function(data) {
+        dataset = data.filter(function(d) { 
+            return d.person == "Adam"; 
+        });
+        console.log(dataset);
+
+        var x = d3.scaleLinear()
+            .domain([2009, 2019])
+            .range([0, width]);
+
+        var y = d3.scaleLinear()
+            .range([height, 0])
+            .domain([8, 1]);
+
+        var xaxis = d3.axisBottom()
+            .scale(x);
+
+        var yaxis = d3.axisLeft()
+            .scale(y);
+
+        var line = d3.line()
+            .x(function(d) { return x(d.year); })
+            .y(function(d) { return y(d.cumrank); })
+            .curve(d3.curveStep);
+
+        var step = svg
+            .append('g')
+            .append("path")
+              .attr("class", "mypath")
+              .datum(dataset)
+              .attr("fill", "none")
+              .attr("opacity", ".8")
+              .attr("stroke", "CORNFLOWERBLUE")
+              .attr("stroke-width", 3)
+              .attr("stroke-linejoin", "round")
+              .attr("d", line);
+
+        var circle = d3.select('svg')
+            .selectAll('circle')
+            .data(dataset)
+            .enter()
+            .append('circle')
+            .attr('cx', function(d) {
+                return x(d.year) + margin.left;
+            })
+            .attr('cy', function(d) {
+                return y(d.cumrank) + margin.top;
+            })
+            .attr('r', 3)
+            .attr("fill", "CORNFLOWERBLUE");
+
+        svg.append("g")
+                .attr("class", "x axis") // assigning an axis attribute (optional)
+                .attr("transform", "translate(0," + (height) + ")")
+                .call(xaxis)
+            .append("text")
+                .attr("x", margin.left/2)
+                .attr("y", -20)
+                .attr("fill", "#000")
+                .attr("text-anchor", "start")
+                .attr("font-family", "Gill Sans")
+                .attr("font-size", "16px")
+                .text("Cumulative points rank");
+
+        svg.append("g")
+            .attr("class", "y axis") // assigning an axis attribute (optional)
+            .attr("transform", "translate(0,0)")
+            .call(yaxis);
+
+        d3.selectAll("button")
+            .on("click", function() {
+
+                var person = d3.select(this).attr("id");
+
+                var color = d3.select(this).attr("color");
+
+                dataset = data.filter(function(d) { 
+                    return d.person == person; 
+                    });
+
+                x.domain([2009, 2019]);
+                y.domain([8, 1]);
+
+                var duration = 1000;
+
+                var line = d3.line()
+                    .x(function(d) { return x(d.year); })
+                    .y(function(d) { return y(d.cumrank); })
+                    .curve(d3.curveStep);
+
+                step
+                    .datum(dataset)
+                    .transition()
+                    .duration(duration)
+                    .attr("d", line)
+                    .attr("stroke", color);
+
+                circle
+                    .data(dataset)
+                    .transition()
+                    .duration(duration)
+                    .attr("cx", function(d) { 
+                        return x(d.year) + margin.left;
+                    })
+                    .attr("cy", function(d) {
+                        return y(d.cumrank) + margin.top;
+                    })
+                    .attr("fill", color);
+
+                svg.select(".x.axis")
+                    .transition()
+                    .duration(duration)
+                    .call(xaxis);
+
+                svg.select(".y.axis")
+                    .transition()
+                    .duration(duration)
+                    .call(yaxis); 
+               }); 
+
+
+});
